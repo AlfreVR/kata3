4e69a2f6b70f8eb8000eba072707b52ceba5a104
AlfredoVR <alfredo2.villarroya@gmail.com>
2024-01-13 20:29:00 +0000

    Added new Class Main

--- src/main/java/software/ulpgc/kata3/Main.java
+++ src/main/java/software/ulpgc/kata3/Main.java
@@ -5,9 +5,30 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    CSVCarLoader carLoader = new CSVCarLoader(new File("CarDatabase.csv"));
-    List<Car> cars = carLoader.load();
-    double[] horsepowerOrganized = new HorsepowerCarOrganizer().organize(cars);
+    public static void main(String[] args) {
 
-}
+        CSVCarLoader carLoader = new CSVCarLoader(new File("CarDatabase.csv"));
+        List<Car> cars = carLoader.load();
+        double[] horsepowerOrganized = new HorsepowerCarOrganizer().organize(cars);
+
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.histogramDisplay().show(histogram(horsepowerOrganized));
+        mainFrame.setVisible(true);
+
+    }
+
+    private static Histogram histogram(double[] horsepowerOrganized) {
+        return new Histogram() {
+            @Override
+            public int bins() {
+                return 10;
+            }
+
+            @Override
+            public double[] values() {
+                return horsepowerOrganized;
+            }
+        };
+    }
+
 }
